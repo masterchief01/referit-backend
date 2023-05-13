cb4a088b35c8feb49f0ed4a5e0674c263b1086c3
Pranjal Walia <pranjalwalia77@gmail.com>
2023-05-13 16:07:37 +0530

    feat: fix sleep

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -1,10 +1,10 @@
 kubectl apply -f infra/k8s/referit-backend-secret.yaml -n referit
 echo "Waiting for Configuration..."
-sleep(10) 
+sleep 5 
 
 kubectl apply -f infra/k8s/referit-backend-configmap.yaml -n referit
 echo "Waiting for Configuration..."
-sleep(10)
+sleep 5
 
 kubectl apply -f infra/k8s/referit-backend-deployment.yaml -n referit
 kubectl rollout restart deployment.apps/referit-backend-deployment -n referit
