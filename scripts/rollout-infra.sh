733ac78061cbb996d3e513f47dd3461c20d50c6c
Pranjal Walia <pranjalwalia77@gmail.com>
2023-05-13 16:04:36 +0530

    feat: update rollout mech

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -1,3 +1,11 @@
-kubectl apply -f infra/k8s -n referit
+kubectl apply -f infra/k8s/referit-backend-secret.yaml -n referit
+echo "Waiting for Configuration..."
+sleep(10) 
+
+kubectl apply -f infra/k8s/referit-backend-configmap.yaml -n referit
+echo "Waiting for Configuration..."
+sleep(10)
+
+kubectl apply -f infra/k8s/referit-backend-deployment.yaml -n referit
 kubectl rollout restart deployment.apps/referit-backend-deployment -n referit
 kubectl rollout status deployment.apps/referit-backend-deployment -n referit
