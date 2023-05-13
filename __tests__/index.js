faed33242e7953ef089ba5529de702d01cb29855
Samaksh <dhingrasamaksh64@gmail.com>
2023-05-13 17:25:01 +0530

    trying to fix prod failure

--- __tests__/index.js
+++ __tests__/index.js
@@ -48,30 +48,30 @@
 // });
 
 
-// const { sumRequest } = require("../controllers/sample");
-// const { sum } = require("../services");
+const { sumRequest } = require("../controllers/sample");
+const { sum } = require("../services");
 
-// describe("Test the sum function and handlers", () => {
-//   test("check the service for /test", () => {
-//     expect(sum(1, 2)).toBe(3);
-//   });
+describe("Test the sum function and handlers", () => {
+  test("check the service for /test", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 
-  // test("check the handler for /test", () => {
-  //   const req = {
-  //     body: {
-  //       a: 1,
-  //       b: 2,
-  //     },
-  //   };
+  test("check the handler for /test", () => {
+    const req = {
+      body: {
+        a: 1,
+        b: 2,
+      },
+    };
 
-  //   const res = {
-  //     text: "",
-  //     json: function (input) {
-  //       this.text = input;
-  //     },
-  //   };
+    const res = {
+      text: "",
+      json: function (input) {
+        this.text = input;
+      },
+    };
 
-  //   sumRequest(req, res);
-  //   expect(res.text).toEqual({ result: 3 });
-  // });
-// });
+    sumRequest(req, res);
+    expect(res.text).toEqual({ result: 3 });
+  });
+});
