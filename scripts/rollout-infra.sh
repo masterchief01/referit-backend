e986969e7db1c786ba923e12768b327bd2eeee1b
Samaksh <dhingrasamaksh64@gmail.com>
2023-05-13 17:57:33 +0530

    trying to fix prod failure

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -6,6 +6,6 @@ kubectl apply -f infra/k8s/referit-backend-configmap.yaml -n referit
 echo "Waiting for Configuration..."
 sleep 5
 
-kubectl apply -f infra/k8s/referit-backend-deployment.yaml -n referit
+kubectl apply -f infra/k8s/referit-backend.yaml -n referit
 kubectl rollout restart deployment.apps/referit-backend-deployment -n referit
-kubectl rollout status deployment.apps/referit-backend -n referit
+kubectl rollout status deployment.apps/referit-backend-deployment -n referit
