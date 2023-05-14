20e16d14b9ebaca821a78b4816ffdf94f9aaac20
Pranjal Walia <pranjalwalia77@gmail.com>
2023-05-14 14:18:59 +0530

    feat: add changes

--- scripts/rollout-infra.sh
+++ scripts/rollout-infra.sh
@@ -1,10 +1,10 @@
-kubectl apply -f infra/k8s/referit-backend-secret.yaml -n referit
-echo "Waiting for Configuration..."
-sleep 5 
+# kubectl apply -f infra/k8s/referit-backend-secret.yaml -n referit
+# echo "Waiting for Configuration..."
+# sleep 5 
 
-kubectl apply -f infra/k8s/referit-backend-configmap.yaml -n referit
-echo "Waiting for Configuration..."
-sleep 5
+# kubectl apply -f infra/k8s/referit-backend-configmap.yaml -n referit
+# echo "Waiting for Configuration..."
+# sleep 5
 
 kubectl apply -f infra/k8s/referit-backend.yaml -n referit
 kubectl rollout restart deployment.apps/referit-backend-deployment -n referit
